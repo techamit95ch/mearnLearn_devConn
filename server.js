3718628da1b393207e08e3d931b61caa6e2326a8
Amit Chakraborty <amit98ch@gmail.com>
2020-09-09 03:17:07 +0530

    Initial Commit

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 const express = require('express');
 const express_validator = require('express-validator');
 const path = require('path');
-const App = express();
+
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/',(req,res)=>res.send('app is fucking'));
+
+app.listen(PORT,()=>{
+    console.log('bal')
+});
