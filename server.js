07f4ff39b3e2e0c8460cfa75c5e4f9af12c97929
Amit Chakraborty <amit98ch@gmail.com>
2020-09-09 04:41:56 +0530

    Routes api creation

--- server.js
+++ server.js
@@ -10,8 +10,14 @@ const PORT = process.env.PORT || 5000;
 
 connectDb();
 
+app.use('/api/users',require('./routes/api/users'));
+
+app.use('/api/auth',require('./routes/api/auth'));
+
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
 app.get('/',(req,res)=>res.send('app is fucking'));
 
-app.listen(PORT,()=>{
+app.listen(PORT,()=>{ 
     console.log('bal')
 });
